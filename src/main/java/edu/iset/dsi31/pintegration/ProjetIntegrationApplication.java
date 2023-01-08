0f9ccb7e28c99c48ff8342ec4c459970d1b10da2
Abdallah Ghabri <abdallah.ghabri@outlook.fr>
2023-01-08 14:29:55 +0100

    first commit

---
+++ src/main/java/edu/iset/dsi31/pintegration/ProjetIntegrationApplication.java
@@ -0,0 +1,13 @@
+package edu.iset.dsi31.pintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iset/dsi31/pintegration/ProjetIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iset.dsi31.pintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
